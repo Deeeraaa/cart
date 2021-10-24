62ae9cda7e0142ca2ce3c8fe2318234656407e12
Dera <stephenie7533@gmail.com>
2021-10-24 02:38:36 +0100

    Updates

---
+++ precache-manifest.d7fc408aed929b00f7ad5e4e5452156c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754fabe2be7cc0327deaee120f5ce18c",
+    "url": "/cart/index.html"
+  },
+  {
+    "revision": "4405fb8ffb39f8fcfadf",
+    "url": "/cart/static/css/main.8db4b5e8.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/cart/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cart/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4405fb8ffb39f8fcfadf",
+    "url": "/cart/static/js/main.c8be4d28.chunk.js"
+  },
+  {
+    "revision": "6f930286e52d694f2280",
+    "url": "/cart/static/js/runtime-main.526553cb.js"
+  }
+]);
